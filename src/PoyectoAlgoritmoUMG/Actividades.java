280f2622745c2f7bfe5c33736b61109e8ba1125a
Jose Avila <javilaa5@miumg.edu.gt>
2022-10-08 22:09:13 -0600

    agregando nuevas clases y atributos

---
+++ src/PoyectoAlgoritmoUMG/Actividades.java
@@ -0,0 +1,13 @@
+package PoyectoAlgoritmoUMG;
+/**
+ *
+ * @author avila
+ */
+public class Actividades {
+    /* la clase actividad se creo para almacenar los objetos de tipo Actividades
+    necesita los atributos nombre de la activida, el estado de la actividad y una 
+    para el identificador unico*/
+    private String nomAct;
+    private String EstadoAct;
+    private int idAct;
+}
---
+++ src/PoyectoAlgoritmoUMG/Lista_Act.java
@@ -0,0 +1,15 @@
+package PoyectoAlgoritmoUMG;
+
+/**
+ *
+ * @author avila
+ */
+public class Lista_Act {
+    /*En la clase Lista Actividades se guardaran los objetos creados de clase lista Activ, 
+    se nesetan los atributos nombre de lista, fecha de la lista y id para le identificador unico*/
+    private String nomlisAct;
+    private String fechaLisAct;
+    private int idLisAct;
+   
+    
+}
